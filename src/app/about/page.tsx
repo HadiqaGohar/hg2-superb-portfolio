410080654e3e3ac515e464a064e4f0acedc48736
HadiqaGohar <tasleemhadiqa76@gmail.com>
2024-10-27 23:05:47 +0500

    update code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ function About() {
     return (
         <div className='custom-container'>
             {/* Image Section with Circular Loader */}
-            <div className='image-section'> {/* Use custom class for the image section */}
+            <div className='image-section'> 
                 <div className='loader-large'></div> {/* First loader */}
                
                 <Image
@@ -14,19 +14,19 @@ function About() {
                     alt="Hadiqa Gohar"
                     height={200}
                     width={200}
-                    className="image" // Use custom class for image
+                    className="image" 
                 />
             </div>
 
               {/* Text Section */}
-              <div className='text-center space-y-4'> {/* Custom classes */}
-                <h1 className='about-heading'> {/* Use custom class for heading */}
-                    About <span className='text-[#19A7CE]'>Me</span> {/* Highlight class for span */}
+              <div className='text-center space-y-4'> 
+                <h1 className='about-heading'> 
+                    About <span className='text-[#19A7CE]'>Me</span> 
                 </h1>
-                <h2 className='frontend-developer'> {/* Use custom class for h2 */}
+                <h2 className='frontend-developer'> 
                     Frontend Developer!
                 </h2>
-                <p className='about-description'> {/* Use custom class for paragraph */}
+                <p className='about-description'> 
                     I am Hadiqa Gohar, a driven and curious senior student at the Governor Initiative, where I&apos;m immersed in the exciting world of Cloud Applied Generative AI, Web 3.0, and Metaverse. With a strong foundation in science and a passion for programming, I&apos;m constantly learning and exploring new possibilities in JavaScript, TypeScript, Python, HTML, CSS, Tailwind CSS, ReactJS, NextJS, and NodeJS. I&apos;m excited to share my journey and projects with you; visit my social media accounts.
                 </p>
             </div>
